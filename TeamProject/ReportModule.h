37e37041f42a21c40de2443086b4ada91fe9963b
Hung Tong <hungtongmx@gmail.com>
2016-03-16 15:07:22 -0700

    Update ReportModule.h

--- TeamProject/ReportModule.h
+++ TeamProject/ReportModule.h
@@ -1,4 +1,14 @@
+/*
+	CIS22B_TeamProject_BookStore
+	Archana Zone
+	Hung Tong
+	Michelle Liu
+	Theodore Fausak
+	Vasuki Sunder
+*/
+
 // Class ReportModule Declaration
+
 #pragma once
 #include "stdafx.h"
 #include<time.h>
