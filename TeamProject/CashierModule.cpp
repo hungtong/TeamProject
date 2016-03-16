f8c2b4c2a03080dbd26c8f3fd237332210c63d49
TedFausak <tmfausak@gmail.com>
2016-03-16 14:21:07 -0700

    CashierModule.cpp
    added comments to cashiermodle

--- TeamProject/CashierModule.cpp
+++ TeamProject/CashierModule.cpp
@@ -12,9 +12,9 @@
 #include "Utils.h"
 #include "BookDAO.h"
 
-
 using namespace std;
 
+//get and define varibles
 CashierModule * CashierModule::cashierModule;
 int CashierModule::numberItems = 0;
 Book CashierModule::booksInCart[1024];
@@ -30,7 +30,7 @@ char * CashierModule::getDatePurchase() {
 	return datePurchase;
 }
 
-CashierModule * CashierModule::getInstance() {
+CashierModule * CashierModule::getInstance() {//chack if nothing entered
 	if (cashierModule == NULL) {
 		cashierModule = new CashierModule();
 	}
@@ -48,23 +48,23 @@ void CashierModule::display() {
 	system("CLS");
 	cout << "Serendipity Book Sellers" << endl << endl;
 	cout << "Date: " << datePurchase << endl << endl;
-	if (numberItems == 0) {
-		cout << "\t You Currently Have No Book In Cart" << endl;
+	if (numberItems == 0) {//check if items in cart
+		cout << "\t You Currently Have No Book In Cart" << endl;//if no tell user to go to inventory module then bing user back to main menu
 		cout << "\t Access Report Module To See A List Of Available Books" << endl;
 		cout << "\t Access Inventory Module To Look Up And Add Books To Cart" << endl;
 		system("pause");
 	}
-	else {
+	else { //if items in cart
 		cout << "\t\t Items In Cart" << endl << endl;
 		for (int i = 0; i < numberItems; i++) 
-			cout << Utils::toString(i + 1, quantities[i], booksInCart[i]) << endl;
+			cout << Utils::convertBookToString(i + 1, quantities[i], booksInCart[i]) << endl;
 
-		cout << "\t\t 1. Checkout" << endl;
+		cout << "\t\t 1. Checkout" << endl;//give user options 1-3
 		cout << "\t\t 2. Delete Items In Cart" << endl;
 		cout << "\t\t 3. Back To Main Menu" << endl << endl;
 		
-		switch (Utils::showChoices(1,3)) {
-		case 1: {
+		switch (Utils::showChoices(1,3)) {//have user inpit choice 1-3
+		case 1: {//if 1: bill user
 			double subtotal = 0.0;
 
 			for (int i = 0; i < numberItems; i++)
@@ -77,7 +77,7 @@ void CashierModule::display() {
 			exit(-1);
 		}
 			break;
-		case 2:
+		case 2://if 2: have user chose book to delete
 			int number = 0;
 
 			cout << "Enter Item Number: ";
@@ -94,13 +94,13 @@ void CashierModule::display() {
 				booksInCart[i] = booksInCart[i + 1];
 
 			numberItems--;
-			cout << "Your Book Has Been Removed From Receipt" << endl;
+			cout << "Your Book Has Been Removed From Receipt" << endl;//inform user of what happened
 			system("pause");
 
 			display();
 			break;
 		}
 	}
-	Module::showMainMenu();	
+	Module::showMainMenu();//bring back to main menu
 	
 }
